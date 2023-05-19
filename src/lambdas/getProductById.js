a720c8bcee74fb2ebf329750cacc10120eadfbe8
Jonathan Rangel <jonathanrangelb@gmail.com>
2023-05-18 20:55:49 -0600

    fix: issue where getProductById was not returning the stock

--- src/lambdas/getProductById.js
+++ src/lambdas/getProductById.js
@@ -1,25 +1,25 @@
 "use strict";
 
 const dynamoInstance = require("../helpers/db");
-module.exports.handler = async (event, context) => {
-  console.info("EVENT\n" + JSON.stringify(event, null, 2));
-  console.log(context.logStreamName);
+module.exports.handler = async (event) => {
   const { productId } = event.pathParameters;
+
+  const productParams = {
+    TableName: process.env.PRODUCTS_TABLE,
+    Key: {
+      id: productId,
+    },
+  };
+  const productStockParams = {
+    TableName: process.env.STOCKS_TABLE,
+    Key: {
+      product_id: productId,
+    },
+  };
+
   try {
-    const product = await dynamoInstance
-      .get({
-        TableName: process.env.PRODUCTS_TABLE,
-        Key: {
-          id: productId,
-        },
-      })
-      .promise();
-    const productStock = await dynamoInstance.get({
-      TableName: process.env.STOCKS_TABLE,
-      Key: {
-        product_id: productId,
-      },
-    });
+    const product = await dynamoInstance.get(productParams).promise();
+    const productStock = await dynamoInstance.get(productStockParams).promise();
     if (!product.Item) {
       throw new Error("Product not found");
     }
